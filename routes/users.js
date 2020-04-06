d1c07097898c1c15f4757e42d2809c9ccbf68501
Rafael F. Silva <rafaelfsilva1@gmail.com>
2020-04-06 12:27:40 -0300

    update docs

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,35 @@ const knex = require('../services/knex');
 const router = express.Router();
 
 /**
- * @api {get} /users Get all users information
- * @apiName GetUser
+ * @api {get} /users Get all
  * @apiGroup User
  *
- * @apiSuccess {Object[]} Users
- *
- * @apiSuccessExample Success-Response:
+ * @apiSuccessExample Response
  *     HTTP/1.1 200 OK
  *     [
  *       {
  *         "id": 1,
+ *         "avatar": null,
  *         "name": "John Doe",
- *         "email": "john.doe@email.com"
+ *         "email": "john.doe@email.com",
+ *         "password": "123",
+ *         "birthday": "1990-01-01",
+ *         "location": "California",
+ *         "description": "Hi, I'm John Doe! Nice to meet you",
+ *         "created_at": "2020-04-03 14:00:00",
+ *         "updated_at": "2020-04-03 14:00:00",
  *       },
  *       {
  *         "id": 2,
- *         "name": "John Doe 2",
- *         "email": "john.doe2@email.com"
+ *         "avatar": null,
+ *         "name": "Monty Python",
+ *         "email": "monty.python@email.com",
+ *         "password": "123",
+ *         "birthday": "1994-01-01",
+ *         "location": "Texas",
+ *         "description": "Hello there",
+ *         "created_at": "2020-04-04 10:00:00",
+ *         "updated_at": "2020-04-04 10:00:00",
  *       }
  *     ]
  */
@@ -32,7 +43,34 @@ router.get('/', function(req, res) {
   });
 });
 
-// Insert new
+/**
+ * @api {post} /users Insert multiple
+ * @apiGroup User
+ * @apiParam (Request Body) {json} User User fields (not all fields are mandatory)
+ * @apiParamExample {json} Example
+ *    {
+ *      "name": "Karen Eliot",
+ *      "email": "karen.eliot@email.com",
+ *      "password": "123",
+ *      "birthday": "1996-01-01",
+ *      "location": "NY",
+ *    }
+ *
+ * @apiSuccessExample Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "id": 3,
+ *       "avatar": null,
+ *       "name": "Karen Eliot",
+ *       "email": "karen.eliot@email.com",
+ *       "password": "123",
+ *       "birthday": "1996-01-01",
+ *       "location": "NY",
+ *       "description": null,
+ *       "created_at": "2020-04-06 18:00:00",
+ *       "updated_at": "2020-04-06 18:00:00",
+ *     }
+ */
 router.post('/', function(req, res) {
   knex('user')
   .insert(req.body)
@@ -44,7 +82,26 @@ router.post('/', function(req, res) {
   });
 });
 
-// Get by id
+/**
+ * @api {get} /users/:id Get a single
+ * @apiGroup User
+ * @apiParam {Number} id User unique ID
+ *
+ * @apiSuccessExample Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "id": 3,
+ *       "avatar": null,
+ *       "name": "Karen Eliot",
+ *       "email": "karen.eliot@email.com",
+ *       "password": "123",
+ *       "birthday": "1996-01-01",
+ *       "location": "NY",
+ *       "description": null,
+ *       "created_at": "2020-04-06 18:00:00",
+ *       "updated_at": "2020-04-06 18:00:00",
+ *     }
+ */
 router.get('/:id', function(req, res) {
   knex('user')
   .where('id', req.params.id)
@@ -53,7 +110,32 @@ router.get('/:id', function(req, res) {
   });
 });
 
-// Update by id
+/**
+ * @api {post} /users/:id Update a single
+ * @apiGroup User
+ * @apiParam {Number} id User unique ID
+ * @apiParam (Request Body) {json} User User fields (not all fields are mandatory)
+ * @apiParamExample {json} Example
+ *    {
+ *      "name": "Netochka Nezvanova",
+ *      "email": "netochka.nezvanova@email.com",
+ *    }
+ *
+ * @apiSuccessExample Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "id": 3,
+ *       "avatar": null,
+ *       "name": "Netochka Nezvanova",
+ *       "email": "netochka.nezvanova@email.com",
+ *       "password": "123",
+ *       "birthday": "1996-01-01",
+ *       "location": "NY",
+ *       "description": null,
+ *       "created_at": "2020-04-06 18:00:00",
+ *       "updated_at": "2020-04-06 18:00:00",
+ *     }
+ */
 router.post('/:id', function(req, res) {
   knex('user')
   .where('id', req.params.id)
@@ -63,6 +145,26 @@ router.post('/:id', function(req, res) {
   })
   .then(function(user) {
     res.json(user);
+  });
+});
+
+/**
+ * @api {delete} /users/:id Delete a single
+ * @apiGroup User
+ * @apiParam {Number} id User unique ID
+ *
+ * @apiSuccessExample Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       3
+ *     ]
+ */
+router.delete('/:id', function(req, res) {
+  knex('user')
+  .where('id', req.params.id)
+  .delete()
+  .then(function(id) {
+    res.json(id);
   });
 });
 
